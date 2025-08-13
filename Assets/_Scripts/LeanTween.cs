009e18732de4004a7013504ecd95d71d18c98122
NQT4192 <163365438+NQT4192@users.noreply.github.com>
2025-08-14 00:43:30 +0700

    fix err after merge 2

--- Assets/_Scripts/LeanTween.cs
+++ Assets/_Scripts/LeanTween.cs
@@ -1,19 +1,19 @@
-using UnityEngine;
-using UnityEngine.EventSystems;
+// using UnityEngine;
+// using UnityEngine.EventSystems;
 
-public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
-{
-    Vector3 startScale;
+// public class ButtonHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+// {
+//     Vector3 startScale;
 
-    void Start() => startScale = transform.localScale;
+//     void Start() => startScale = transform.localScale;
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        LeanTween.scale(gameObject, startScale * 1.1f, 0.1f).setEaseOutBack();
-    }
+//     public void OnPointerEnter(PointerEventData eventData)
+//     {
+//         LeanTween.scale(gameObject, startScale * 1.1f, 0.1f).setEaseOutBack();
+//     }
 
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        LeanTween.scale(gameObject, startScale, 0.1f).setEaseInBack();
-    }
-}
+//     public void OnPointerExit(PointerEventData eventData)
+//     {
+//         LeanTween.scale(gameObject, startScale, 0.1f).setEaseInBack();
+//     }
+// }
